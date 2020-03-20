62de81825178eb82b9336974a81a24d60f1501b6
Arius Bronte <ariusbronte@gmail.com>
2020-03-20 20:34:06 +0000

    cleanup code: added XML comments to initial code

--- src/SimplyArchitecture.WebApi/Program.cs
+++ src/SimplyArchitecture.WebApi/Program.cs
@@ -9,18 +9,32 @@ using Microsoft.Extensions.Logging;
 
 namespace SimplyArchitecture.WebApi
 {
+    /// <summary>
+    ///     Entry point class of programme.
+    /// </summary>
     public class Program
     {
-        public static void Main(string[] args)
+        /// <summary>
+        ///     Entry method of programme.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            await CreateHostBuilder(args).Build().RunAsync();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Microsoft.Extensions.Hosting.HostBuilder"/> class with pre-configured defaults.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>The <see cref="Microsoft.Extensions.Hosting.IHostBuilder"/> so that additional calls can be chained.</returns>
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
--- src/SimplyArchitecture.WebApi/Startup.cs
+++ src/SimplyArchitecture.WebApi/Startup.cs
@@ -13,22 +13,29 @@ using Microsoft.Extensions.Logging;
 
 namespace SimplyArchitecture.WebApi
 {
+    /// <summary>
+    ///      Configures services and the app's request pipeline.
+    /// </summary>
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services">Defines a contract for a collection of service descriptors.</param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        ///     This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app">
+        ///     Provides the mechanisms to configure an application's request pipeline.
+        /// </param>
+        /// <param name="env">
+        ///     Provides information about the web hosting environment an application is running in.
+        /// </param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,11 +44,8 @@ namespace SimplyArchitecture.WebApi
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
