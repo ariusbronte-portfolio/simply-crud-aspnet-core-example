ea66024ac10f46105f522747c2016754f9612e68
Arius Bronte <ariusbronte@gmail.com>
2020-03-21 02:17:38 +0000

    Create PersonDto.cs

---
+++ src/SimplyArchitecture.WebApi/Dto/PersonDto.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SimplyArchitecture.WebApi.Validators;
+
+namespace SimplyArchitecture.WebApi.Dto
+{
+    /// <summary>
+    ///     Presents the essence of the persons.
+    /// </summary>
+    public class PersonDto
+    {
+        /// <summary>
+        ///     Gets the primary key for this person.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        ///     Gets person firstname.
+        /// </summary>
+        [Required]
+        [StringLength(128)]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        ///     Gets person lastname.
+        /// </summary>
+        [Required]
+        [StringLength(128)]
+        public string LastName { get; set; }
+
+        /// <summary>
+        ///     Gets person age.
+        /// </summary>
+        [GreaterThan(0)]
+        public int Age { get; set; }
+    }
+}
