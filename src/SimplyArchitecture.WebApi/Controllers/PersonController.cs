b87458451bd9bcd84017cd8f47cc6e2f4264a12a
Arius Bronte <ariusbronte@gmail.com>
2020-03-21 02:18:52 +0000

    Create PersonController.cs

---
+++ src/SimplyArchitecture.WebApi/Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SimplyArchitecture.WebApi.Abstractions;
+using SimplyArchitecture.WebApi.Domain;
+using SimplyArchitecture.WebApi.Dto;
+
+namespace SimplyArchitecture.WebApi.Controllers
+{
+    /// <summary>
+    ///     Controller for working with persons.
+    /// </summary>
+    [ApiController]
+    [Route(template: "[controller]")]
+    [Produces(contentType: "application/json")]
+    public class PersonController : AbstractController<PersonEntity, PersonDto>
+    {
+        /// <inheritdoc />
+        public PersonController(IRepository<PersonEntity> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
