cc8d54d000d51d64f3b4d8201110632275e8f59c
Arius Bronte <ariusbronte@gmail.com>
2020-03-21 21:41:18 +0000

    create GreaterThanTest

---
+++ test/SimplyArchitecture.Tests.Unit/Validators/GreaterThanTest.cs
@@ -0,0 +1,53 @@
+﻿using SimplyArchitecture.WebApi.Validators;
+using Xunit;
+
+namespace SimplyArchitecture.Tests.Unit.Validators
+{
+    /// <inheritdoc cref="SimplyArchitecture.WebApi.Validators.GreaterThan"/>
+    public class GreaterThanTest
+    {
+        [Fact]
+        public void GreaterThan_GreaterThanZero_SuccessValidation()
+        {
+            // Arrange
+            const int arg = 0;
+            const int arg1 = 1;
+            
+            // Act
+            var greaterThan = new GreaterThan(arg);
+            var validation = greaterThan.IsValid(arg1);
+            
+            // Arrange
+            Assert.True(validation);
+        }
+        
+        [Fact]
+        public void GreaterThan_EqualZero_FailedValidation()
+        {
+            // Arrange
+            const int arg = 0;
+            
+            // Act
+            var greaterThan = new GreaterThan(arg);
+            var validation = greaterThan.IsValid(arg);
+            
+            // Arrange
+            Assert.False(validation);
+        }
+
+        [Fact]
+        public void GreaterThan_GreaterThanZero_FailedValidation()
+        {
+            // Arrange
+            const int arg = 0;
+            const int arg1 = -1;
+            
+            // Act
+            var greaterThan = new GreaterThan(arg);
+            var validation = greaterThan.IsValid(arg1);
+            
+            // Arrange
+            Assert.False(validation);
+        }
+    }
+}
