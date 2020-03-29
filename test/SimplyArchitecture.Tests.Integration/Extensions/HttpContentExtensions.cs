85616b854d87a9089f59ed3a2a4a7646b089ff00
Arius Bronte <ariusbronte@gmail.com>
2020-03-29 21:11:12 +0000

    Create HttpContentExtensions.cs

---
+++ test/SimplyArchitecture.Tests.Integration/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SimplyArchitecture.Tests.Integration.Extensions
+{
+    /// <summary>
+    ///     Extension methods for <see cref="System.Net.Http.HttpContent" />.
+    /// </summary>
+    public static class HttpContentExtensions
+    {
+        /// <summary>
+        ///     Cast the HTTP entity body to the specified {T}.
+        /// </summary>
+        /// <param name="content">A base class that represents the content headers and body of the HTTP entity.</param>
+        /// <typeparam name="T">Object to cast the HTTP request body.</typeparam>
+        public static async Task<T> ReadAsAsync<T>(this HttpContent content)
+        {
+            if (content == null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+            
+            return JsonConvert.DeserializeObject<T>(value: await content.ReadAsStringAsync());
+        }
+    }
+}
