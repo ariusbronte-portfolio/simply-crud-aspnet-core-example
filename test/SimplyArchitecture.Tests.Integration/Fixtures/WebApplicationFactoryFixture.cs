879adb1378ea564cfa574861da1c48297f5cf6a0
Arius Bronte <ariusbronte@gmail.com>
2020-03-29 20:57:15 +0000

    Create WebApplicationFactoryFixture.cs

---
+++ test/SimplyArchitecture.Tests.Integration/Fixtures/WebApplicationFactoryFixture.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using SimplyArchitecture.WebApi;
+using SimplyArchitecture.WebApi.DataAccess;
+using SimplyArchitecture.WebApi.HostedServices;
+
+namespace SimplyArchitecture.Tests.Integration.Fixtures
+{
+    /// <inheritdoc />
+    public class WebApplicationFactoryFixture<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        /// <inheritdoc />
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<SimplyArchitectureDbContext>));
+
+                services.AddDbContext<SimplyArchitectureDbContext>(optionsAction: options =>
+                {
+                    options.UseInMemoryDatabase(databaseName: "DataSource=:memory:");
+                });
+
+                services.AddHostedService<MigratorHostedService>();
+            });
+        }
+    }
+
+    /// <inheritdoc />
+    public class WebApplicationFactoryFixture : WebApplicationFactoryFixture<Startup>
+    {
+    }
+}
